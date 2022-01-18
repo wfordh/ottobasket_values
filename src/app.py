41caa498851304ee9c467149b7be572d78422452
wfordh <ford.higgins@doma.com>
2022-01-18 12:54:12 -0800

    add position multiselect

--- src/app.py
+++ src/app.py
@@ -19,8 +19,12 @@ format_cols = {
 }
 player_input = st.sidebar.text_input("Player name", placeholder="Stephen Curry").lower()
 if player_input:
-    st.write("Dataframe has been filtered!")
     values_df = values_df.loc[values_df.player.str.lower().str.contains(player_input)]
+position_input = st.sidebar.multiselect(
+    "Positon", options=values_df.ottoneu_position.unique()
+)
+if position_input:
+    values_df = values_df.loc[values_df.ottoneu_position.isin(position_input)]
 display_df = (
     values_df.drop(["nba_player_id", "ottoneu_player_id", "tm_id"], axis=1)
     .drop_duplicates()
