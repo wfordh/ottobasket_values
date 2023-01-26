04512b320e8d4c7866829c44d787397bc8ba3ddd
Ford Higgins <wfordh@users.noreply.github.com>
2023-01-26 15:24:10 -0800

    get rankings data and infer actual projected games RoS for hashtag basketball

--- src/ros_minutes_projections.py
+++ src/ros_minutes_projections.py
@@ -10,6 +10,7 @@ import time
 
 import gspread
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -32,13 +33,14 @@ def _shutdown_chrome_scraper(driver: webdriver.firefox.webdriver.WebDriver) -> N
     driver.quit()
 
 
-def _get_projections_page(driver: webdriver.firefox.webdriver.WebDriver) -> str:
+def _get_projections_page(
+    url: str, driver: webdriver.firefox.webdriver.WebDriver
+) -> str:
     """
     Pulls the content of the projections page and reloads it after selecting the
     correct dropdown to return all players instead of the initially given top
     200, with a sleep period added for courtesy and to prevent blacklisting.
     """
-    url = "https://hashtagbasketball.com/fantasy-basketball-projections"
     driver.get(url)
     time.sleep(1.2)
     dropdown = Select(driver.find_element(By.ID, "ContentPlaceHolder1_DDSHOW"))
@@ -49,7 +51,26 @@ def _get_projections_page(driver: webdriver.firefox.webdriver.WebDriver) -> str:
     return content
 
 
-def _extract_projections(content: str) -> pd.DataFrame:
+def _get_player_projection_data(
+    row_data: list, name_idx: int, game_idx: int, minutes_idx: int
+) -> dict:
+    player_data = dict()
+    player_data["name"] = row_data[name_idx].a.text.strip()
+    player_data["pid"] = row_data[name_idx].a["href"].split("/")[1]
+    player_data["games_forecast"] = str(row_data[game_idx].text.strip())
+    player_data["minutes_forecast"] = str(row_data[minutes_idx].text.strip())
+    return player_data
+
+
+def _get_player_ytd_data(row_data: list, name_idx: int, game_idx: int) -> dict:
+    """Year to date minutes and games played from the rankings page."""
+    player_data = dict()
+    player_data["pid"] = row_data[name_idx].a["href"].split("/")[1]
+    player_data["games_played"] = str(row_data[game_idx].text.strip())
+    return player_data
+
+
+def _extract_projections(is_projections: bool, content: str) -> pd.DataFrame:
     """
     Extracts the projections for each player from the provided page. It pulls
     and cleans the name, player ID (for mapping), and forecasts for minutes and
@@ -62,25 +83,58 @@ def _extract_projections(content: str) -> pd.DataFrame:
     )
     all_players = list()
 
+    headers = rows.pop(0)
+    headers = [th.get_text().strip().upper() for th in headers.find_all("th")]
+    NAME_INDEX = headers.index("PLAYER")
+    GP_INDEX = headers.index("GP")
+    MINUTES_INDEX = headers.index("MPG")
+
     for row in rows:
         if row.find("td") is None or row.find("b"):
             continue
         row_data = row.find_all("td")
-        player_data = dict()
+        # player_data = dict()
+
         try:
-            player_data["name"] = row_data[1].a.text.strip()
-            player_data["pid"] = row_data[1].a["href"].split("/")[1]
-            player_data["games_forecast"] = str(row_data[4].text.strip())
-            player_data["minutes_forecast"] = str(row_data[5].text.strip())
+            if is_projections:
+                player_data = _get_player_projection_data(
+                    row_data, NAME_INDEX, GP_INDEX, MINUTES_INDEX
+                )
+            else:
+                player_data = _get_player_ytd_data(row_data, NAME_INDEX, GP_INDEX)
             all_players.append(player_data)
-        except:
+        except Exception as e:
+            # should probably add logging here...
             # specify error. do I want to do anything?
-            pass  # I think?
+            print(player_data, e)
     return pd.DataFrame(all_players).dropna(axis=0)
 
 
+def _get_fantasypros_projections() -> pd.DataFrame:
+    """
+    Currently no player IDs in this data so skipping as I solved the issue
+    with hashtag data. Could revisit and map it later
+    """
+    url = "https://www.fantasypros.com/nba/projections/ros-overall.php"
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    headers = [th.get_text().strip() for th in soup.find("thead").find_all("th")]
+    rows = soup.find("tbody").find_all("tr")
+    data = []
+    for row in rows:
+        row_data = []
+        for idx, td in enumerate(row.find_all("td")):
+            if idx == 0:
+                row_data.append(td.a.text.strip())
+            else:
+                row_data.append(td.get_text().strip())
+        data.append(row_data)
+
+    df = pd.DataFrame(data, columns=headers)
+    return df
+
+
 def _setup_gdrive(client_key_string: str) -> gspread.client.Client:
-    print(client_key_string)
     credentials = json.loads(client_key_string)
     return gspread.service_account_from_dict(credentials)
 
@@ -97,11 +151,24 @@ def _upload_data(gc: gspread.client.Client, data: pd.DataFrame) -> None:
 def main():
     client_key_string = os.environ.get("SERVICE_BLOB", None)
     driver = _setup_chrome_scraper()
-    content = _get_projections_page(driver)
-    data = _extract_projections(content)
-    print("got projections")
-    gc = _setup_gdrive(client_key_string)
-    _upload_data(gc, data)
+    projections_url = "https://hashtagbasketball.com/fantasy-basketball-projections"
+    rankings_url = "https://hashtagbasketball.com/fantasy-basketball-rankings"
+    proj_content = _get_projections_page(projections_url, driver)
+    rankings_content = _get_projections_page(rankings_url, driver)
+    proj_data = _extract_projections(True, proj_content)
+    rankings_data = _extract_projections(False, rankings_content)
+    try:
+        data = proj_data.merge(rankings_data, how="left", on="pid")
+        data.fillna(0, inplace=True)
+        data["games_forecast"] = data.games_forecast.astype(
+            int
+        ) - data.games_played.astype(int)
+        print("got projections")
+        gc = _setup_gdrive(client_key_string)
+        _upload_data(gc, data)
+    except (KeyError, TypeError) as e:
+        # again should prob use logging
+        print(e)
     _shutdown_chrome_scraper(driver)
     print("done!")
 
