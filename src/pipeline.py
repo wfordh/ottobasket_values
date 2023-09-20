44bfa888f3627eae514d2c250965f8d69dbaebe8
Ford Higgins <wfordh@users.noreply.github.com>
2023-09-20 09:31:17 -0700

    started removing FS stuff from pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -21,9 +21,6 @@ def ottobasket_values_pipeline(
 ) -> Union[None, pd.DataFrame]:
     stats_df = prep_stats_df()
 
-    # full strength
-    full_strength_df = get_scoring_minutes_combo("full_strength", stats_df)
-
     # current
     current_minutes_df = get_scoring_minutes_combo("current", stats_df)
 
@@ -40,20 +37,12 @@ def ottobasket_values_pipeline(
         "tm_id",
         "ottoneu_position",
         "minutes",
-        "fs_min",
         "total_ros_minutes",
         "minutes_ytd",
     ]
-    all_values_df = (
-        current_minutes_df.merge(
-            full_strength_df,
-            how="inner",
-            on=join_cols,
-            suffixes=["_current", "_fs"],
-        )
-        .merge(ros_df, how="left", on=join_cols, suffixes=["", "_ros"])
-        .merge(ytd_df, how="left", on=join_cols, suffixes=["", "_ytd"])
-    )
+    all_values_df = current_minutes_df.merge(
+        ros_df, how="left", on=join_cols, suffixes=["", "_ros"]
+    ).merge(ytd_df, how="left", on=join_cols, suffixes=["", "_ytd"])
     all_values_df.rename(
         columns={
             "simple_points_value": "simple_points_value_ros",
