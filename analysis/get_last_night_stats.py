c2543b5d9c188a9e37e96b2fa897c53f1e7a69bc
Ford Higgins <wfordh@users.noreply.github.com>
2026-02-27 22:28:40 -0800

    fix nightly run

--- analysis/get_last_night_stats.py
+++ analysis/get_last_night_stats.py
@@ -7,6 +7,7 @@ import pandas as pd
 sys.path.append(os.path.abspath("src"))
 
 from leagues import get_league_leaderboard
+from utils import _setup_gdrive, _upload_data
 
 LEAGUE_ID = 39  # can be any trad pts league
 
@@ -16,8 +17,14 @@ def main():
 
     leaders = get_league_leaderboard(
         league_id=LEAGUE_ID, start_date=yesterday, end_date=yesterday
-    )
-    leaders.to_csv(sys.stdout, index=False)
+    ).fillna("")
+
+    # leaders.to_csv("./data/last_night_test.csv")
+    sheet_key = "1TOfcNsLBEZRP2GuXdu2oIqh6HnYf-hy4O6eqh15afCM"
+    client_key_string = os.environ.get("SERVICE_BLOB", None)
+    gc = _setup_gdrive(client_key_string)
+
+    _upload_data(gc, leaders, sheet_key, wks_num=0, clear=True)
 
 
 if __name__ == "__main__":
